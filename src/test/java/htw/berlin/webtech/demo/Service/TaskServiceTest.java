7b1f2760ba0a6bfae3d10ac5e00bbf8889f00795
Dang Khanh Le <danglekhanh2003@gmail.com>
2023-01-08 20:21:09 +0100

    added TaskService Test

---
+++ src/test/java/htw/berlin/webtech/demo/Service/TaskServiceTest.java
@@ -0,0 +1,53 @@
+package htw.berlin.webtech.demo.Service;
+
+import htw.berlin.webtech.demo.persistence.TaskRepository;
+import htw.berlin.webtech.demo.service.TaskService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceTest {
+
+    @Mock
+    private TaskRepository repository;
+
+    @InjectMocks
+    private TaskService underTest;
+
+    @Test
+    @DisplayName("should return true if delete was successful")
+    void should_return_true_if_delete_was_successful() {
+        // given
+        Long givenId = 111L;
+        doReturn(true).when(repository).existsById(givenId);
+
+        // when
+        boolean result = underTest.deleteById(givenId);
+
+        // then
+        verify(repository).deleteById(givenId);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("should return false if task to delete does not exist")
+    void should_return_false_if_task_to_delete_does_not_exist() {
+        // given
+        Long givenId = 111L;
+        doReturn(false).when(repository).existsById(givenId);
+
+        // when
+        boolean result = underTest.deleteById(givenId);
+
+        // then
+        verifyNoMoreInteractions(repository);
+        assertThat(result).isFalse();
+    }
+}
